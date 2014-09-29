563c2e2830f3d7fcb3384bb8f9fd86df8a009d11
HimelRahman <himel0925@gmail.com>
2014-09-29 13:09:59 +0600

    4th assignment

---
+++ BankinterestApp/BankinterestApp/InterestccalculationUI.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace BankinterestApp
+{
+    partial class InterestccalculationUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.outputlabel = new System.Windows.Forms.Label();
+            this.weekComboBox = new System.Windows.Forms.ComboBox();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.balanceTextBox = new System.Windows.Forms.TextBox();
+            this.outputButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bank Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time(in year)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Balance";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Interest =";
+            // 
+            // outputlabel
+            // 
+            this.outputlabel.AutoSize = true;
+            this.outputlabel.Location = new System.Drawing.Point(127, 205);
+            this.outputlabel.Name = "outputlabel";
+            this.outputlabel.Size = new System.Drawing.Size(0, 13);
+            this.outputlabel.TabIndex = 4;
+            // 
+            // weekComboBox
+            // 
+            this.weekComboBox.FormattingEnabled = true;
+            this.weekComboBox.Items.AddRange(new object[] {
+            "BRAC",
+            "DBBL",
+            "HSBC"});
+            this.weekComboBox.Location = new System.Drawing.Point(109, 34);
+            this.weekComboBox.Name = "weekComboBox";
+            this.weekComboBox.Size = new System.Drawing.Size(121, 21);
+            this.weekComboBox.TabIndex = 5;
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(109, 79);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(121, 20);
+            this.timeTextBox.TabIndex = 6;
+            // 
+            // balanceTextBox
+            // 
+            this.balanceTextBox.Location = new System.Drawing.Point(109, 127);
+            this.balanceTextBox.Name = "balanceTextBox";
+            this.balanceTextBox.Size = new System.Drawing.Size(121, 20);
+            this.balanceTextBox.TabIndex = 7;
+            // 
+            // outputButton
+            // 
+            this.outputButton.Location = new System.Drawing.Point(81, 163);
+            this.outputButton.Name = "outputButton";
+            this.outputButton.Size = new System.Drawing.Size(149, 23);
+            this.outputButton.TabIndex = 8;
+            this.outputButton.Text = "Calculate Interest";
+            this.outputButton.UseVisualStyleBackColor = true;
+            this.outputButton.Click += new System.EventHandler(this.outputButton_Click);
+            // 
+            // InterestccalculationUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.outputButton);
+            this.Controls.Add(this.balanceTextBox);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.weekComboBox);
+            this.Controls.Add(this.outputlabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "InterestccalculationUI";
+            this.Text = "Interest Calculate From";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label outputlabel;
+        private System.Windows.Forms.ComboBox weekComboBox;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.TextBox balanceTextBox;
+        private System.Windows.Forms.Button outputButton;
+    }
+}
+
---
+++ BankinterestApp/BankinterestApp/InterestccalculationUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankinterestApp
+{
+    public partial class InterestccalculationUI : Form
+    {
+        public InterestccalculationUI()
+        {
+            InitializeComponent();
+        }
+
+        string name;
+        int year;
+        double balance;
+        double store;
+        private void outputButton_Click(object sender, EventArgs e)
+        {
+            name = weekComboBox.Text;
+            year = Convert.ToInt32(timeTextBox.Text);
+            balance = Convert.ToDouble(balanceTextBox.Text);
+
+            if (name == "BRAC")
+             store =(((balance*6)*year)/100);
+            else if (name == "DBBL")
+                store = (((balance * 7) * year) / 100);
+            else if (name == "HSBC")
+                store = (((balance * 8) * year) / 100);
+
+            outputlabel.Text =store.ToString();
+        }
+    }
+}
