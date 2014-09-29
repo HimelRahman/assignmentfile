703138248ef5fd184b368bd1d7fc93a94dc8990b
HimelRahman <himel0925@gmail.com>
2014-09-29 13:16:19 +0600

    6th assignment

---
+++ Itemcount/Itemcount/ItemCountUI.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Itemcount
+{
+    partial class ItemCountUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.countLabe = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.outputlistBox = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.countLabe);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.resetButton);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.inputTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(36, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(403, 170);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Name";
+            // 
+            // countLabe
+            // 
+            this.countLabe.AutoSize = true;
+            this.countLabe.Location = new System.Drawing.Point(206, 125);
+            this.countLabe.Name = "countLabe";
+            this.countLabe.Size = new System.Drawing.Size(35, 13);
+            this.countLabe.TabIndex = 4;
+            this.countLabe.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Add Count :";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(285, 77);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(285, 32);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(19, 32);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(220, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(321, 219);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 5;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // outputlistBox
+            // 
+            this.outputlistBox.FormattingEnabled = true;
+            this.outputlistBox.Location = new System.Drawing.Point(55, 219);
+            this.outputlistBox.Name = "outputlistBox";
+            this.outputlistBox.Size = new System.Drawing.Size(220, 95);
+            this.outputlistBox.TabIndex = 4;
+            // 
+            // ItemCountUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 337);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.outputlistBox);
+            this.Name = "ItemCountUI";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label countLabe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListBox outputlistBox;
+    }
+}
+
---
+++ Itemcount/Itemcount/ItemCountUI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Itemcount
+{
+    public partial class ItemCountUI : Form
+    {
+        public ItemCountUI()
+        {
+            InitializeComponent();
+        }
+        int index = 0;
+        List<string> list = new List<string>();
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            list.Add(inputTextBox.Text);
+            index += 1;
+            inputTextBox.Text = " ";
+            countLabe.Text = Convert.ToString(index);
+            if (index == 10)
+            {
+                addButton.Enabled = false;
+                outputlistBox.Items.Clear();
+                foreach (string name in list)
+                {
+                    outputlistBox.Items.Add(name);
+                }
+
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            outputlistBox.Items.Clear();
+            addButton.Enabled = true;
+            index = 0;
+            countLabe.Text = " ";
+            list.Clear();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            outputlistBox.Items.Clear();
+            if (list.Count == 0)
+                MessageBox.Show("Insert item first");
+            foreach(string store in list)
+                outputlistBox.Items.Add(store);
+        }
+    }
+}
