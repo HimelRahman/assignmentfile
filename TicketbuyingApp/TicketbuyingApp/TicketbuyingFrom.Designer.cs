58445ccae2aaffd5ac97fd5b69fa9f1f3a96962c
HimelRahman <himel0925@gmail.com>
2014-09-29 13:27:04 +0600

    ticket activity's related app

---
+++ TicketbuyingApp/TicketbuyingApp/TicketbuyingFrom.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TicketbuyingApp
+{
+    partial class TicketbuyingFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.detailshowButton = new System.Windows.Forms.Button();
+            this.priceButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numberTextBox);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.detailshowButton);
+            this.groupBox1.Controls.Add(this.priceButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 226);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer And Ticket Information";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(127, 86);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(229, 20);
+            this.numberTextBox.TabIndex = 6;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(127, 43);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(229, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // detailshowButton
+            // 
+            this.detailshowButton.Location = new System.Drawing.Point(231, 166);
+            this.detailshowButton.Name = "detailshowButton";
+            this.detailshowButton.Size = new System.Drawing.Size(125, 23);
+            this.detailshowButton.TabIndex = 4;
+            this.detailshowButton.Text = "Details";
+            this.detailshowButton.UseVisualStyleBackColor = true;
+            this.detailshowButton.Click += new System.EventHandler(this.detailshowButton_Click);
+            // 
+            // priceButton
+            // 
+            this.priceButton.Location = new System.Drawing.Point(50, 166);
+            this.priceButton.Name = "priceButton";
+            this.priceButton.Size = new System.Drawing.Size(145, 23);
+            this.priceButton.TabIndex = 3;
+            this.priceButton.Text = "Total Price";
+            this.priceButton.UseVisualStyleBackColor = true;
+            this.priceButton.Click += new System.EventHandler(this.priceButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "One Ticket 10 Taka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Tickets";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // TicketbuyingFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TicketbuyingFrom";
+            this.Text = "Ticket Selling UI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button detailshowButton;
+        private System.Windows.Forms.Button priceButton;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+    }
+}
+
---
+++ TicketbuyingApp/TicketbuyingApp/TicketbuyingFrom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketbuyingApp
+{
+    public partial class TicketbuyingFrom : Form
+    {
+        public TicketbuyingFrom()
+        {
+            InitializeComponent();
+        }
+        string name;
+        double price=1.0;
+        double item;
+        private void priceButton_Click(object sender, EventArgs e)
+        {
+            item= Convert.ToDouble(numberTextBox.Text);
+            price =item * 10.00;
+            MessageBox.Show("your total price is " +price);
+        }
+
+        private void detailshowButton_Click(object sender, EventArgs e)
+        {
+            name = nameTextBox.Text;
+            clearall();
+            MessageBox.Show(name +", you buy " + item +" tickets and total price of ticket is " + price);
+        }
+
+        private void clearall()
+        {
+            nameTextBox.Text = string.Empty;
+            numberTextBox.Text = string.Empty;
+        }
+
+        
+
+    
+    }
+}
