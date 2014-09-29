3c89ddf78a1cfb303adfeccdff548f039025906e
HimelRahman <himel0925@gmail.com>
2014-09-29 13:08:26 +0600

    3rd assignment

---
+++ arraywith list/arraywith list/StudentgradesheetUI.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace arraywith_list
+{
+    partial class StudentgradesheetUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rolltextBox = new System.Windows.Forms.TextBox();
+            this.banglatextBox = new System.Windows.Forms.TextBox();
+            this.englishtextBox = new System.Windows.Forms.TextBox();
+            this.mathtextBox = new System.Windows.Forms.TextBox();
+            this.sciencetextBox = new System.Windows.Forms.TextBox();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.showbutton = new System.Windows.Forms.Button();
+            this.showlistBox = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.edTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bangla";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "English";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Math";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Scince";
+            // 
+            // rolltextBox
+            // 
+            this.rolltextBox.Location = new System.Drawing.Point(113, 13);
+            this.rolltextBox.Name = "rolltextBox";
+            this.rolltextBox.Size = new System.Drawing.Size(116, 20);
+            this.rolltextBox.TabIndex = 5;
+            // 
+            // banglatextBox
+            // 
+            this.banglatextBox.Location = new System.Drawing.Point(113, 44);
+            this.banglatextBox.Name = "banglatextBox";
+            this.banglatextBox.Size = new System.Drawing.Size(116, 20);
+            this.banglatextBox.TabIndex = 6;
+            // 
+            // englishtextBox
+            // 
+            this.englishtextBox.Location = new System.Drawing.Point(113, 78);
+            this.englishtextBox.Name = "englishtextBox";
+            this.englishtextBox.Size = new System.Drawing.Size(116, 20);
+            this.englishtextBox.TabIndex = 7;
+            // 
+            // mathtextBox
+            // 
+            this.mathtextBox.Location = new System.Drawing.Point(113, 108);
+            this.mathtextBox.Name = "mathtextBox";
+            this.mathtextBox.Size = new System.Drawing.Size(116, 20);
+            this.mathtextBox.TabIndex = 8;
+            // 
+            // sciencetextBox
+            // 
+            this.sciencetextBox.Location = new System.Drawing.Point(113, 137);
+            this.sciencetextBox.Name = "sciencetextBox";
+            this.sciencetextBox.Size = new System.Drawing.Size(116, 20);
+            this.sciencetextBox.TabIndex = 9;
+            // 
+            // savebutton
+            // 
+            this.savebutton.Location = new System.Drawing.Point(275, 13);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(75, 23);
+            this.savebutton.TabIndex = 10;
+            this.savebutton.Text = "Save";
+            this.savebutton.UseVisualStyleBackColor = true;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Enabled = false;
+            this.editButton.Location = new System.Drawing.Point(275, 365);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 23);
+            this.editButton.TabIndex = 11;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // showbutton
+            // 
+            this.showbutton.Location = new System.Drawing.Point(275, 78);
+            this.showbutton.Name = "showbutton";
+            this.showbutton.Size = new System.Drawing.Size(75, 23);
+            this.showbutton.TabIndex = 12;
+            this.showbutton.Text = "Show";
+            this.showbutton.UseVisualStyleBackColor = true;
+            this.showbutton.Click += new System.EventHandler(this.showbutton_Click);
+            // 
+            // showlistBox
+            // 
+            this.showlistBox.FormattingEnabled = true;
+            this.showlistBox.Location = new System.Drawing.Point(50, 181);
+            this.showlistBox.Name = "showlistBox";
+            this.showlistBox.Size = new System.Drawing.Size(300, 160);
+            this.showlistBox.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Give ur roll";
+            // 
+            // edTextBox
+            // 
+            this.edTextBox.Location = new System.Drawing.Point(139, 365);
+            this.edTextBox.Name = "edTextBox";
+            this.edTextBox.Size = new System.Drawing.Size(100, 20);
+            this.edTextBox.TabIndex = 15;
+            // 
+            // StudentgradesheetUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 415);
+            this.Controls.Add(this.edTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.showlistBox);
+            this.Controls.Add(this.showbutton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.sciencetextBox);
+            this.Controls.Add(this.mathtextBox);
+            this.Controls.Add(this.englishtextBox);
+            this.Controls.Add(this.banglatextBox);
+            this.Controls.Add(this.rolltextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentgradesheetUI";
+            this.Text = "GradepointCacculation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox rolltextBox;
+        private System.Windows.Forms.TextBox banglatextBox;
+        private System.Windows.Forms.TextBox englishtextBox;
+        private System.Windows.Forms.TextBox mathtextBox;
+        private System.Windows.Forms.TextBox sciencetextBox;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button showbutton;
+        private System.Windows.Forms.ListBox showlistBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox edTextBox;
+    }
+}
+
---
+++ arraywith list/arraywith list/StudentgradesheetUI.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arraywith_list
+{
+    public partial class StudentgradesheetUI : Form
+    {
+        public StudentgradesheetUI()
+        {
+            InitializeComponent();
+        }
+
+        string roll;
+        string ban;
+        string eng;
+        String math;
+        string snc;
+        string [,] arr = new string[28,85];
+        int row = 0;
+        int col = 0;
+        int cl = 0;
+        private void savebutton_Click(object sender, EventArgs e)
+        {
+            editButton.Enabled = true;
+            roll = rolltextBox.Text;
+            ban = banglatextBox.Text;
+            eng = englishtextBox.Text;
+            math = mathtextBox.Text;
+            snc = sciencetextBox.Text;
+
+            rolltextBox.Text =" ";
+            banglatextBox.Text =" ";
+            englishtextBox.Text = " ";
+            mathtextBox.Text = " ";
+            sciencetextBox.Text = " ";
+
+            arr[row, col++] = roll;
+            arr[row, col++] = ban;
+            arr[row, col++] = eng;
+            arr[row, col++] = math;
+            arr[row, col]= snc;
+            cl = col;
+            row += 1; col = 0;
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            int id=0,i;
+            int j = 0;
+            roll = edTextBox.Text;
+            MessageBox.Show("roll for edit: " + roll);
+            for (id=0;id<row;id+=1)
+            {
+                if ((roll.Trim()) == (arr[id,0].Trim()))                //Convert.ToInt32(arr[i, 0]) == (Convert.ToInt32(roll))
+                {
+                    MessageBox.Show(arr[id,0]);
+                    break; 
+                }
+            }
+            rolltextBox.Text = arr[id, j++];
+            banglatextBox.Text = arr[id, j++];
+            englishtextBox.Text = arr[id, j++];
+            mathtextBox.Text = arr[id, j++];
+            sciencetextBox.Text = arr[id, j++];
+
+            editButton.Enabled = false; 
+        }
+
+        private void showbutton_Click(object sender, EventArgs e)
+        {
+            showlistBox.Items.Clear();
+
+            for (int i = 0; i < row; i += 1)
+                for (int j = 0; j <= cl; j += 1)
+                    showlistBox.Items.Add(arr[i, j]);
+        }
+    }
+}
